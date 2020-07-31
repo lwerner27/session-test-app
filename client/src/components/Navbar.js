8a2898b0319dd4af6d35b7c58f730f071dbc986e
Lucas Werner <34554883+lwerner27@users.noreply.github.com>
2020-07-31 16:31:46 -0500

    Added icon to the navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = ({ toggleBurger, burgerActive }) => {
     return (
@@ -8,55 +10,59 @@ const Navbar = ({ toggleBurger, burgerActive }) => {
             role='navigation'
             aria-label='main navigation'
         >
-            <div className='navbar-brand'>
-                <Link as='a' className='navbar-item' to='/'>
-                    <h1 className='is-size-5'>
-                        <strong>Express Session Test</strong>
-                    </h1>
-                </Link>
+            <div className='container'>
+                <div className='navbar-brand'>
+                    <Link as='a' className='navbar-item' to='/'>
+                        <h1 className='is-size-5'>
+                            <strong>Express Session Test</strong>
+                        </h1>
+                    </Link>
 
-                <Link
-                    as='a'
-                    role='button'
-                    className={`navbar-burger burger ${
+                    <Link
+                        as='a'
+                        role='button'
+                        className={`navbar-burger burger ${
+                            burgerActive ? 'is-active' : ''
+                        }`}
+                        aria-label='menu'
+                        aria-expanded='false'
+                        data-target='navbarBasicExample'
+                        onClick={toggleBurger}
+                        to='#'
+                    >
+                        <span aria-hidden='true'></span>
+                        <span aria-hidden='true'></span>
+                        <span aria-hidden='true'></span>
+                    </Link>
+                </div>
+
+                <div
+                    id='navbarBasicExample'
+                    className={`navbar-menu is-link ${
                         burgerActive ? 'is-active' : ''
                     }`}
-                    aria-label='menu'
-                    aria-expanded='false'
-                    data-target='navbarBasicExample'
-                    onClick={toggleBurger}
-                    to='#'
                 >
-                    <span aria-hidden='true'></span>
-                    <span aria-hidden='true'></span>
-                    <span aria-hidden='true'></span>
-                </Link>
-            </div>
-
-            <div
-                id='navbarBasicExample'
-                className={`navbar-menu is-link ${
-                    burgerActive ? 'is-active' : ''
-                }`}
-            >
-                <div className='navbar-end'>
-                    <div className='navbar-item'>
-                        <div className='buttons'>
-                            <Link
-                                to='/registration'
-                                as='a'
-                                className='button is-white'
-                            >
-                                Register
-                            </Link>
-                            <Link
-                                to='/login'
-                                as='a'
-                                className='button is-light'
-                            >
-                                Login
-                            </Link>
+                    <div className='navbar-end'>
+                        <div className='navbar-item'>
+                            <div className='buttons'>
+                                <Link
+                                    to='/registration'
+                                    as='a'
+                                    className='button is-white'
+                                >
+                                    Register
+                                </Link>
+                            </div>
                         </div>
+
+                        <Link className='navbar-item' as='a' to='/login'>
+                            <span className='icon fas fa-lg'>
+                                <FontAwesomeIcon icon={faSignInAlt} />
+                            </span>
+                            <span className='ml-2'>
+                                <strong>Login</strong>
+                            </span>
+                        </Link>
                     </div>
                 </div>
             </div>
