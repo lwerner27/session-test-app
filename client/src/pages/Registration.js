0031d2ad1bd2dc729476b10bde52e978869d25e2
Lucas Werner <34554883+lwerner27@users.noreply.github.com>
2020-08-03 20:16:09 -0500

    Fixed registration page so it uses bulma styles.

--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { withRouter } from "react-router-dom";
 
 class Registration extends React.Component {
     constructor(props) {
@@ -23,24 +23,25 @@ class Registration extends React.Component {
 
     // Handles Changes to the form inputs
     handleChange(event) {
+        console.log(event.target.name);
         this.setState({ [event.target.name]: event.target.value });
     }
 
     checkForNullValues() {
-        if (this.state.firstName === null || '') {
-            return 'Please enter a value for the first name field.';
-        } else if (this.state.lastName === null || '') {
-            return 'Please enter a value for the last name field.';
-        } else if (this.state.username === null || '') {
-            return 'Please enter a value for the username field.';
-        } else if (this.state.password === null || '') {
-            return 'Please enter a value for the password field.';
-        } else if (this.state.confirmPassword === null || '') {
-            return 'Please enter a value for the confirm password field.';
+        if (this.state.firstName === null || "") {
+            return "Please enter a value for the first name field.";
+        } else if (this.state.lastName === null || "") {
+            return "Please enter a value for the last name field.";
+        } else if (this.state.username === null || "") {
+            return "Please enter a value for the username field.";
+        } else if (this.state.password === null || "") {
+            return "Please enter a value for the password field.";
+        } else if (this.state.confirmPassword === null || "") {
+            return "Please enter a value for the confirm password field.";
         } else if (this.state.location === null) {
-            return 'Please select a location.';
+            return "Please select a location.";
         } else if (this.state.role === null) {
-            return 'Please select a role.';
+            return "Please select a role.";
         } else {
             return false;
         }
@@ -48,11 +49,11 @@ class Registration extends React.Component {
 
     checkPassword() {
         if (this.state.password.length < 8) {
-            return 'Please choose a password that is at least 8 characters.';
+            return "Please choose a password that is at least 8 characters.";
         }
 
         if (this.state.password !== this.state.confirmPassword) {
-            return 'Passwords do not match.';
+            return "Passwords do not match.";
         }
 
         return false;
@@ -72,10 +73,10 @@ class Registration extends React.Component {
             return;
         }
 
-        fetch('/auth/register', {
-            method: 'POST',
+        fetch("/auth/register", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
             body: JSON.stringify(this.state),
         })
@@ -83,7 +84,7 @@ class Registration extends React.Component {
             .then((res) => {
                 if (res.success) {
                     alert(res.msg);
-                    this.props.history.push('/');
+                    this.props.history.push("/");
                 } else {
                     alert(res.msg);
                 }
@@ -92,132 +93,184 @@ class Registration extends React.Component {
 
     render() {
         return (
-            <div className='container'>
-                <div className='row mt-5 justify-content-center'>
-                    <div className='col-md-6 col-sm-12'>
-                        <div className='card'>
-                            <div className='card-body'>
-                                <form>
-                                    <div className='form-row'>
-                                        <div className='form-group col-md-6 col-sm-12'>
-                                            <label htmlFor='fistName'>
-                                                First Name:
-                                            </label>
+            <div className="container is-fluid">
+                <div className="columns mt-5 is-centered">
+                    <div className="column is-half">
+                        <form>
+                            <div className="columns">
+                                <div className="column">
+                                    <div className="field">
+                                        <label
+                                            htmlFor="firstName"
+                                            className="label"
+                                        >
+                                            First Name:
+                                        </label>
+                                        <div className="control">
                                             <input
-                                                type='text'
-                                                id='firstName'
-                                                className='form-control'
-                                                name='firstName'
-                                                onChange={this.handleChange}
-                                            />
-                                        </div>
-                                        <div className='form-group col-md-6 col-sm-12'>
-                                            <label htmlFor='lastName'>
-                                                Last Name:
-                                            </label>
-                                            <input
-                                                className='form-control'
-                                                type='text'
-                                                name='lastName'
-                                                id='lastName'
+                                                type="text"
+                                                className="input"
+                                                name="firstName"
+                                                id="firstName"
                                                 onChange={this.handleChange}
                                             />
                                         </div>
                                     </div>
-                                    <div className='form-row'>
-                                        <div className='form-group col'>
-                                            <label htmlFor='username'>
-                                                Username:
-                                            </label>
+                                </div>
+                                <div className="column">
+                                    <div className="field">
+                                        <label
+                                            htmlFor="lastName"
+                                            className="label"
+                                        >
+                                            Last Name:
+                                        </label>
+                                        <div className="control">
                                             <input
-                                                type='text'
-                                                className='form-control'
-                                                name='username'
-                                                id='username'
+                                                type="text"
+                                                className="input"
+                                                name="lastName"
+                                                id="lastName"
                                                 onChange={this.handleChange}
                                             />
                                         </div>
                                     </div>
-                                    <div className='form-row'>
-                                        <div className='form-group col'>
-                                            <label htmlFor='password'>
-                                                Password:
-                                            </label>
+                                </div>
+                            </div>
+                            <div className="columns">
+                                <div className="column">
+                                    <div className="field">
+                                        <label
+                                            htmlFor="username"
+                                            className="label"
+                                        >
+                                            Username:
+                                        </label>
+                                        <div className="control">
                                             <input
-                                                type='password'
-                                                className='form-control'
-                                                name='password'
-                                                id='password'
+                                                type="text"
+                                                className="input"
+                                                name="username"
+                                                id="username"
                                                 onChange={this.handleChange}
                                             />
                                         </div>
                                     </div>
-                                    <div className='form-row'>
-                                        <div className='form-group col'>
-                                            <label htmlFor='confirmPassword'>
-                                                Confirm Password:
-                                            </label>
+                                </div>
+                            </div>
+                            <div className="columns">
+                                <div className="column">
+                                    <div className="field">
+                                        <label
+                                            htmlFor="password"
+                                            className="label"
+                                        >
+                                            Password:
+                                        </label>
+                                        <div className="control">
                                             <input
-                                                type='password'
-                                                className='form-control'
-                                                name='confirmPassword'
-                                                id='confirmPassword'
+                                                type="password"
+                                                className="input"
+                                                name="password"
+                                                id="password"
                                                 onChange={this.handleChange}
                                             />
                                         </div>
                                     </div>
-                                    <div className='form-row'>
-                                        <div className='form-group col'>
-                                            <label htmlFor='location'>
-                                                Location:
-                                            </label>
+                                </div>
+                            </div>
+                            <div className="columns">
+                                <div className="column">
+                                    <div className="field">
+                                        <label
+                                            htmlFor="confirmPassword"
+                                            className="label"
+                                        >
+                                            Confirm Password:
+                                        </label>
+                                        <div className="control">
+                                            <input
+                                                type="password"
+                                                className="input"
+                                                name="confirmPassword"
+                                                id="confirmPassword"
+                                                onChange={this.handleChange}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="columns">
+                                <div className="column">
+                                    <div className="field">
+                                        <label
+                                            htmlFor="location"
+                                            className="label"
+                                        >
+                                            Location
+                                        </label>
+                                        <div className="select is-fullwidth">
                                             <select
-                                                id='location'
-                                                name='location'
-                                                className='form-control'
-                                                defaultValue='Choose One...'
+                                                defaultValue="Choose Location..."
+                                                name="location"
+                                                id="location"
                                                 onChange={this.handleChange}
                                             >
-                                                <option disabled>
-                                                    Choose One...
+                                                <option
+                                                    disabled
+                                                    value="Choose Location..."
+                                                >
+                                                    Choose Location...
                                                 </option>
-                                                <option value='MEL'>MEL</option>
-                                                <option value='KEL'>KEL</option>
-                                                <option value='PEL'>PEL</option>
+                                                <option value="KEL">KEL</option>
+                                                <option value="MEL">MEL</option>
+                                                <option value="PEL">PEL</option>
                                             </select>
                                         </div>
                                     </div>
-                                    <div className='form-row'>
-                                        <div className='form-group col'>
-                                            <label htmlFor='role'>Role</label>
+                                </div>
+                            </div>
+                            <div className="columns">
+                                <div className="column">
+                                    <div className="field">
+                                        <label htmlFor="role" className="label">
+                                            Role:
+                                        </label>
+                                        <div className="select is-fullwidth">
                                             <select
-                                                id='role'
-                                                name='role'
-                                                className='form-control'
-                                                defaultValue='Choose One...'
+                                                defaultValue="Choose Role..."
+                                                name="role"
+                                                id="role"
                                                 onChange={this.handleChange}
                                             >
-                                                <option disabled>
-                                                    Choose One...
+                                                <option
+                                                    disabled
+                                                    value="Choose Role..."
+                                                >
+                                                    Choose Role...
                                                 </option>
-                                                <option value='Administrator'>
-                                                    Administrator
+                                                <option value="admin">
+                                                    Admin
                                                 </option>
-                                                <option value='Standard User'>
+                                                <option value="Standard User">
                                                     Standard User
                                                 </option>
                                             </select>
                                         </div>
                                     </div>
+                                </div>
+                            </div>
+                            <div className="columns">
+                                <div className="column">
                                     <button
+                                        className="button is-link is-fullwidth"
                                         onClick={this.submitRegistration}
-                                        className='btn btn-primary btn-block'
                                     >
                                         Submit
                                     </button>
-                                </form>
+                                </div>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
